cd483aaf6cc72f8b08b696d0eb8814764a0a214f
adlezz <135785353+adlezz@users.noreply.github.com>
2025-01-22 00:16:22 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
     exclude: ['lucide-react'],
   },
   server: {
-    port: process.env.PORT || 3000,
+    host: '0.0.0.0',
+    port: process.env.PORT || 3000, 
   },
 });
